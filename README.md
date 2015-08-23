e1d69ed28d8a77cde7729d832f6426a2e59d3eba
Ryan Lewis <ryan@wpyz.org>
2015-08-23 13:01:30 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# URL Factory (Proposal)
+
+This is just a jot-down of some ideas of a better way of dealing with 
+URL rewrites for the MS platform.
+
+Some ideas:
+
+* Fluent configuration interface
+* CSX configuration files
+* HTTP module, OWIN interface
+
---
+++ src/UrlFactory.Core/UrlFactoryModule.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace UrlFactory.Core
+{
+    public class UrlFactoryModule : IHttpModule
+    {
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += Context_BeginRequest;
+        }
+
+        private void Context_BeginRequest(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            HttpContext context = application.Context;
+
+            //context.Response.Redirect()
+        }
+    }
+}
